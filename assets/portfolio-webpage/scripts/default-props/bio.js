852017c17fdf6041069ca731867e550bfb3f4e4d
Ethan Frost <ethan.michael.frost@gmail.com>
2019-05-27 14:48:21 -0700

    Update estimated savings

--- assets/portfolio-webpage/scripts/default-props/bio.js
+++ assets/portfolio-webpage/scripts/default-props/bio.js
@@ -5,7 +5,7 @@
 const bio = {
   text: [
     "Ethan Frost here! I'm a New England born fantasy author turned web developer, currently having a blast building the full-stack web apps for Free Code Camp's back-end certification.",
-    "My day job is in retail, but I'm already finding ways to apply my new skills. I worked with IT to develop a SQL app that saves the store an est. $5,600/yr in labor (and improves sales!).",
+    "My day job is in retail, but I'm already finding ways to apply my new skills. I worked with IT to develop a SQL app that saves the company an est. $54,000/yr in labor (and improves sales!).",
     "---",
     "Tip: Projects are in chronological order, so skip to the end of each section to see the most interesting ones."
   ],
