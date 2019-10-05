87794a99521b474592c704734a6c57c0216626c0
Ethan Frost <ethan.michael.frost@gmail.com>
2019-10-05 01:11:22 -0700

    Connect routes

--- assets/nightlife-app/routes.js
+++ assets/nightlife-app/routes.js
@@ -1,10 +1,29 @@
 "use strict";
 
+//local imports
+
+const apiRouter = require("./scripts/server/api/api");
+
+//global imports
+
+const { handleSession } = require("redux/server-utils");
+
 //node modules
 
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const router = express.Router();
+
+//middleware
+
+router.use(bodyParser.json());
+
+handleSession(router);
+
+//api handlers
+
+router.use("/api", apiRouter);
 
 //home page
 
