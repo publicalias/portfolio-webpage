f129ce254ddd34370f2974cf762719979848dd75
Ethan Frost <ethan.michael.frost@gmail.com>
2019-11-07 16:46:41 -0800

    Create VenueLogo component

--- assets/nightlife-app/scripts/client/components/main/venue/page/photos/venue-logo.jsx
+++ assets/nightlife-app/scripts/client/components/main/venue/page/photos/venue-logo.jsx
@@ -1,8 +1,22 @@
 "use strict";
 
+//node modules
+
+const React = require("react");
+
 //venue logo
 
-const VenueLogo = () => null;
+const VenueLogo = () => (
+  <a href="https://www.yelp.com/">
+    <img
+      alt="Yelp Logo"
+      className="c-venue-photos__logo"
+      src="/nightlife-app/media/yelp-logo.png"
+    />
+  </a>
+);
+
+VenueLogo.propList = [];
 
 //exports
 
