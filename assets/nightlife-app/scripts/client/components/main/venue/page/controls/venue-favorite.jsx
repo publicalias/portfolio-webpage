d1fffe3a24ef2fe786266a13b72fe2052c2bb6c2
Ethan Frost <ethan.michael.frost@gmail.com>
2019-11-09 15:56:46 -0800

    Create VenueFavorite component

--- assets/nightlife-app/scripts/client/components/main/venue/page/controls/venue-favorite.jsx
+++ assets/nightlife-app/scripts/client/components/main/venue/page/controls/venue-favorite.jsx
@@ -1,8 +1,51 @@
 "use strict";
 
+//node modules
+
+const React = require("react");
+
 //venue favorite
 
-const VenueFavorite = () => null;
+const VenueFavorite = (props) => {
+
+  const {
+    actions: { favoriteAdd, favoriteRemove },
+    data: { user },
+    local: { refresh, venue }
+  } = props;
+
+  //utilities
+
+  const favorite = venue.favorites.find((e) => e.user.id === user.id);
+
+  //events
+
+  const handleClick = async () => {
+
+    if (favorite) {
+      await favoriteRemove(favorite.id);
+    } else {
+      await favoriteAdd(venue.name, venue.id);
+    }
+
+    refresh();
+
+  };
+
+  //render
+
+  return (
+    <button
+      className="c-icon-button qa-toggle-favorite"
+      onClick={handleClick}
+    >
+      <i className={favorite ? "fas fa-star" : "far fa-star"} />
+    </button>
+  );
+
+};
+
+VenueFavorite.propList = ["data.user", "local"];
 
 //exports
 
--- assets/nightlife-app/tests/client/components/main/venue/page/controls/venue-favorite.test.jsx
+++ assets/nightlife-app/tests/client/components/main/venue/page/controls/venue-favorite.test.jsx
@@ -4,17 +4,17 @@
 
 const VenueFavorite = require("../../../../../../../scripts/client/components/main/venue/page/controls/venue-favorite");
 
+const { newUserWithData, newVenue } = require("../../../../../../../schemas");
 const { testWrapper } = require("../../../../../test-helpers");
 
 //global imports
 
-const { initTestSnapshot, reactTests } = require("redux/tests/react-tests");
+const { testMock } = require("redux/tests/meta-tests");
+const { initTestEvent, initTestSnapshot, reactTests, withDataList } = require("redux/tests/react-tests");
 
 //utilities
 
 const { testShallow } = testWrapper(VenueFavorite);
-
-const testSnapshot = initTestSnapshot(testShallow);
 
 //setup
 
@@ -23,4 +23,64 @@ beforeEach(reactTests.inject(VenueFavorite));
 
 //venue favorite
 
-test("venue favorite should match snapshot", () => testSnapshot());
+describe("venue favorite", () => {
+
+  const testFavorite = withDataList(testShallow, [null, {
+    venue: newVenue({
+      id: "id-a",
+      name: "Venue A"
+    })
+  }]);
+
+  const testSnapshot = initTestSnapshot(testFavorite);
+
+  const testClick = initTestEvent(testFavorite, "click");
+
+  it("should match snapshot", () => testSnapshot());
+
+  it("should call handleClick on click", () => testClick(
+    ".qa-toggle-favorite",
+    [null, { refresh: jest.fn() }],
+    (props) => {
+
+      const { actions: { favoriteAdd }, local: { refresh } } = props;
+
+      testMock(favoriteAdd, ["Venue A", "id-a"]);
+      testMock(refresh, []);
+
+    }
+  ));
+
+});
+
+describe("venue favorite (favorite)", () => {
+
+  const testFavorite = withDataList(testShallow, [{ user: newUserWithData({ id: "id-a" }) }, {
+    venue: newVenue({
+      favorites: [{
+        id: "id-b",
+        user: { id: "id-a" }
+      }]
+    })
+  }]);
+
+  const testSnapshot = initTestSnapshot(testFavorite);
+
+  const testClick = initTestEvent(testFavorite, "click");
+
+  it("should match snapshot", () => testSnapshot());
+
+  it("should call handleClick on click", () => testClick(
+    ".qa-toggle-favorite",
+    [null, { refresh: jest.fn() }],
+    (props) => {
+
+      const { actions: { favoriteRemove }, local: { refresh } } = props;
+
+      testMock(favoriteRemove, ["id-b"]);
+      testMock(refresh, []);
+
+    }
+  ));
+
+});
